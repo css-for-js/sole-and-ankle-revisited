6b1206b0b0d1905daea0e8b8c5b111796f1f3a89
Joshua Comeau <joshwcomeau@gmail.com>
2021-05-02 15:52:47 -0400

    Exercise 5 solution

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -56,8 +56,8 @@ const MainHeader = styled.div`
   display: flex;
   align-items: baseline;
   padding: 18px 32px;
-  height: 72px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+  overflow: auto;
 
   @media ${QUERIES.tabletAndSmaller} {
     justify-content: space-between;
@@ -73,7 +73,7 @@ const MainHeader = styled.div`
 
 const DesktopNav = styled.nav`
   display: flex;
-  gap: 48px;
+  gap: clamp(1rem, 9.2vw - 4.5rem, 3.5rem);
   margin: 0px 48px;
 
   @media ${QUERIES.tabletAndSmaller} {

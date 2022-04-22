13d2f7c70e06da1afbb35ae77adebddc6e642d64
Camilo Arias <jcamiloat@gmail.com>
2022-04-22 09:28:16 -0500

    Ex1: Breakpoints setup

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import styled from 'styled-components/macro';
+import { ThemeProvider } from 'styled-components';
 
 import Header from '../Header';
 import ShoeIndex from '../ShoeIndex';
+import { QUERIES } from '../../constants';
 
 const App = () => {
-  const [sortId, setSortId] = React.useState('newest');
+    const [sortId, setSortId] = React.useState('newest');
 
-  return (
-    <>
-      <Header />
-      <Main>
-        <ShoeIndex sortId={sortId} setSortId={setSortId} />
-      </Main>
-    </>
-  );
+    return (
+        <ThemeProvider theme={{ queries: QUERIES }}>
+            <Header />
+            <Main>
+                <ShoeIndex sortId={sortId} setSortId={setSortId} />
+            </Main>
+        </ThemeProvider>
+    );
 };
 
 const Main = styled.main`
-  padding: 64px 32px;
+    padding: 64px 32px;
 `;
 
 export default App;
--- src/constants.js
+++ src/constants.js
@@ -1,18 +1,30 @@
 export const COLORS = {
-  white: 'hsl(0deg 0% 100%)',
-  gray: {
-    100: 'hsl(185deg 5% 95%)',
-    300: 'hsl(190deg 5% 80%)',
-    500: 'hsl(196deg 4% 60%)',
-    700: 'hsl(220deg 5% 40%)',
-    900: 'hsl(220deg 3% 20%)',
-  },
-  primary: 'hsl(340deg 65% 47%)',
-  secondary: 'hsl(240deg 60% 63%)',
+    white: 'hsl(0deg 0% 100%)',
+    gray: {
+        100: 'hsl(185deg 5% 95%)',
+        300: 'hsl(190deg 5% 80%)',
+        500: 'hsl(196deg 4% 60%)',
+        700: 'hsl(220deg 5% 40%)',
+        900: 'hsl(220deg 3% 20%)',
+    },
+    primary: 'hsl(340deg 65% 47%)',
+    secondary: 'hsl(240deg 60% 63%)',
 };
 
 export const WEIGHTS = {
-  normal: 500,
-  medium: 600,
-  bold: 800,
+    normal: 500,
+    medium: 600,
+    bold: 800,
+};
+
+export const BREAKPOINTS = {
+    mobileMax: 600,
+    tabletMax: 950,
+    laptopMax: 1300,
+};
+
+export const QUERIES = {
+    mobileAndDown: `(max-width: ${BREAKPOINTS.mobileMax / 16}rem)`,
+    tabletAndDown: `(max-width: ${BREAKPOINTS.tabletMax / 16}rem)`,
+    laptopAndDown: `(max-width: ${BREAKPOINTS.laptopMax / 16}rem)`,
 };

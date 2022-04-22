b39598ff9d90f92ea2701ec0df8f36e9ce5e63ff
Camilo Arias <jcamiloat@gmail.com>
2022-04-22 13:53:01 -0500

    Ex2: My Solution

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,69 +5,85 @@ import { COLORS, WEIGHTS } from '../../constants';
 import Logo from '../Logo';
 import SuperHeader from '../SuperHeader';
 import MobileMenu from '../MobileMenu';
+import Icon from '../Icon';
 
 const Header = () => {
-  const [showMobileMenu, setShowMobileMenu] = React.useState(false);
+    const [showMobileMenu, setShowMobileMenu] = React.useState(false);
 
-  // For our mobile hamburger menu, we'll want to use a button
-  // with an onClick handler, something like this:
-  //
-  // <button onClick={() => setShowMobileMenu(true)}>
+    // For our mobile hamburger menu, we'll want to use a button
+    // with an onClick handler, something like this:
+    //
+    // <button onClick={() => setShowMobileMenu(true)}>
 
-  return (
-    <header>
-      <SuperHeader />
-      <MainHeader>
-        <Side>
-          <Logo />
-        </Side>
-        <Nav>
-          <NavLink href="/sale">Sale</NavLink>
-          <NavLink href="/new">New&nbsp;Releases</NavLink>
-          <NavLink href="/men">Men</NavLink>
-          <NavLink href="/women">Women</NavLink>
-          <NavLink href="/kids">Kids</NavLink>
-          <NavLink href="/collections">Collections</NavLink>
-        </Nav>
-        <Side />
-      </MainHeader>
-
-      <MobileMenu
-        isOpen={showMobileMenu}
-        onDismiss={() => setShowMobileMenu(false)}
-      />
-    </header>
-  );
+    return (
+        <header>
+            <SuperHeader />
+            <MainHeader>
+                <Side>
+                    <Logo />
+                </Side>
+                <Nav>
+                    <NavLink href="/sale">Sale</NavLink>
+                    <NavLink href="/new">New&nbsp;Releases</NavLink>
+                    <NavLink href="/men">Men</NavLink>
+                    <NavLink href="/women">Women</NavLink>
+                    <NavLink href="/kids">Kids</NavLink>
+                    <NavLink href="/collections">Collections</NavLink>
+                </Nav>
+                <MobileNav>
+                    <Icon id="shopping-bag" strokeWidth={2} size={24} />
+                    <Icon id="search" strokeWidth={2} size={24} />
+                    <Icon id="menu" strokeWidth={2} size={24} />
+                </MobileNav>
+            </MainHeader>
+            <MobileMenu
+                isOpen={showMobileMenu}
+                onDismiss={() => setShowMobileMenu(false)}
+            />
+        </header>
+    );
 };
 
 const MainHeader = styled.div`
-  display: flex;
-  align-items: baseline;
-  padding: 18px 32px;
-  height: 72px;
-  border-bottom: 1px solid ${COLORS.gray[300]};
+    display: flex;
+    align-items: baseline;
+    padding: 18px 32px;
+    height: 72px;
+    border-bottom: 1px solid ${COLORS.gray[300]};
 `;
 
 const Nav = styled.nav`
-  display: flex;
-  gap: 48px;
-  margin: 0px 48px;
+    display: flex;
+    gap: 48px;
+    margin: 0px 48px;
+
+    @media ${(p) => p.theme.queries.tabletAndDown} {
+        display: none;
+    }
 `;
 
 const Side = styled.div`
-  flex: 1;
+    flex: 1;
 `;
 
 const NavLink = styled.a`
-  font-size: 1.125rem;
-  text-transform: uppercase;
-  text-decoration: none;
-  color: ${COLORS.gray[900]};
-  font-weight: ${WEIGHTS.medium};
+    font-size: 1.125rem;
+    text-transform: uppercase;
+    text-decoration: none;
+    color: ${COLORS.gray[900]};
+    font-weight: ${WEIGHTS.medium};
 
-  &:first-of-type {
-    color: ${COLORS.secondary};
-  }
+    &:first-of-type {
+        color: ${COLORS.secondary};
+    }
+`;
+
+const MobileNav = styled.nav`
+    display: none;
+    @media ${(p) => p.theme.queries.tabletAndDown} {
+        display: flex;
+        gap: 4vw;
+    }
 `;
 
 export default Header;
--- src/components/SuperHeader/SuperHeader.js
+++ src/components/SuperHeader/SuperHeader.js
@@ -8,45 +8,66 @@ import UnstyledButton from '../UnstyledButton';
 import Icon from '../Icon';
 
 const SuperHeader = () => {
-  return (
-    <Wrapper>
-      <MarketingMessage>
-        Free shipping on domestic orders over $75!
-      </MarketingMessage>
-      <SearchInput />
-      <HelpLink href="/help">Help</HelpLink>
-      <UnstyledButton>
-        <Icon id="shopping-bag" strokeWidth={1} />
-      </UnstyledButton>
-    </Wrapper>
-  );
+    return (
+        <>
+            <BigScreensHeader>
+                <MarketingMessage>
+                    Free shipping on domestic orders over $75!
+                </MarketingMessage>
+                <SearchInput />
+                <HelpLink href="/help">Help</HelpLink>
+                <UnstyledButton>
+                    <Icon id="shopping-bag" strokeWidth={1} />
+                </UnstyledButton>
+            </BigScreensHeader>
+            <SmallScreensHeader></SmallScreensHeader>
+        </>
+    );
 };
 
-const Wrapper = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 24px;
-  font-size: 0.875rem;
-  color: ${COLORS.gray[300]};
-  background-color: ${COLORS.gray[900]};
-  height: 40px;
-  padding-left: 32px;
-  padding-right: 32px;
+// const Wrapper = styled.div``;
+
+const BigScreensHeader = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 24px;
+    font-size: 0.875rem;
+    color: ${COLORS.gray[300]};
+    background-color: ${COLORS.gray[900]};
+    height: 40px;
+    padding-left: 32px;
+    padding-right: 32px;
+
+    @media ${(p) => p.theme.queries.tabletAndDown} {
+        display: none;
+    }
+`;
+
+const SmallScreensHeader = styled.hr`
+    display: none;
+    margin: 0;
+    border: 0;
+
+    @media ${(p) => p.theme.queries.tabletAndDown} {
+        display: revert;
+        height: 4px;
+        background-color: ${COLORS.gray[900]};
+    }
 `;
 
 const MarketingMessage = styled.span`
-  color: ${COLORS.white};
-  margin-right: auto;
+    color: ${COLORS.white};
+    margin-right: auto;
 `;
 
 const HelpLink = styled.a`
-  color: inherit;
-  text-decoration: none;
-  outline-offset: 2px;
+    color: inherit;
+    text-decoration: none;
+    outline-offset: 2px;
 
-  &:not(:focus-visible) {
-    outline: none;
-  }
+    &:not(:focus-visible) {
+        outline: none;
+    }
 `;
 
 export default SuperHeader;

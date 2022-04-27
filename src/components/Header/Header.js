0dac83e12eba948541a6a4503b3352ba6efdd528
Camilo Arias <jcamiloat@gmail.com>
2022-04-27 13:33:17 -0500

    Fixing header padding on Mobile

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -50,6 +50,10 @@ const MainHeader = styled.div`
     padding: 18px 32px;
     height: 72px;
     border-bottom: 1px solid ${COLORS.gray[300]};
+
+    @media ${(p) => p.theme.queries.mobileAndDown} {
+        padding: 18px 16px;
+    }
 `;
 
 const Nav = styled.nav`

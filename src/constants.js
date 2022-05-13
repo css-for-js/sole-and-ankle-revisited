7e7f702dbe847b2620c1ebfbfd1402834e2d273f
Dennis Stötzel <github@meeq.de>
2022-05-13 18:03:02 +0200

    breakpoints

--- src/constants.js
+++ src/constants.js
@@ -16,3 +16,14 @@ export const WEIGHTS = {
   medium: 600,
   bold: 800,
 };
+
+const BREAKPOINTS = {
+  tabletMin: 37.5,
+  laptopMin: 59.375,
+  desktopMin: 81.25,
+};
+const QUERIES = {
+  phone: `(max-width: ${BREAKPOINTS.tabletMin}rem)`,
+  tabletAndDown: `(max-width: ${BREAKPOINTS.laptopMin}rem)`,
+  laptopAndDown: `(max-width: ${BREAKPOINTS.desktopMin}rem)`,
+};
